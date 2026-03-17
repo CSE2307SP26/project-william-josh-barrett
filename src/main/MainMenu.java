dde02bb5b3f7f014270f58ff2fcb70092a09da56
William Vargas <vargas@wustl.edu>
2026-03-17 16:37:18 -0500

    Added account creation to main menu

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -1,59 +1,118 @@
 package main;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MainMenu {
 
-    private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+    private static final int EMPTY = 0;
+    private static enum startSelections {MIN, CREATE, EXIT, MAX}
+    private static enum accountSelections {MIN, DEPOSIT, SWITCH, CREATE, EXIT, MAX}
 
-	private BankAccount userAccount;
+    private ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
     private Scanner keyboardInput;
+    private boolean exit = false;
+    private int curAccount;
 
     public MainMenu() {
-        this.userAccount = new BankAccount();
         this.keyboardInput = new Scanner(System.in);
+    }
+
+    public int scanInt() {
+        int integer = keyboardInput.nextInt();
+        keyboardInput.nextLine();
+        return integer;
     }
 
     public void displayOptions() {
         System.out.println("Welcome to the 237 Bank App!");
         
-        System.out.println("1. Make a deposit");
-        System.out.println("2. Exit the app");
-
+        if (accounts.size() == EMPTY) {
+            System.out.println("1. Create an account");
+            System.out.println("2. Exit the app");
+        } else {
+            System.out.println("1. Make a deposit");
+            System.out.println("2. Switch accounts");
+            System.out.println("3. Create an account");
+            System.out.println("4. Exit the app");
+        }
     }
 
     public int getUserSelection(int max) {
         int selection = -1;
-        while(selection < 1 || selection > max) {
+        while(selection < 1 || selection >= max) {
             System.out.print("Please make a selection: ");
-            selection = keyboardInput.nextInt();
+            selection = scanInt();
         }
         return selection;
     }
 
-    public void processInput(int selection) {
-        switch (selection) {
-            case 1:
-                performDeposit();
-        }
+    public void createAccount() {
+        System.out.print("Please enter the account name: ");
+        String name = keyboardInput.nextLine();
+        BankAccount new_account = new BankAccount(name);
+        accounts.add(new_account);
+        curAccount = accounts.size() - 1;
     }
 
     public void performDeposit() {
         double depositAmount = -1;
         while(depositAmount < 0) {
             System.out.print("How much would you like to deposit: ");
-            depositAmount = keyboardInput.nextInt();
+            depositAmount = scanInt();
         }
-        userAccount.deposit(depositAmount);
+        accounts.get(curAccount).deposit(depositAmount);
+    }
+
+    public void switchAccounts() {
+        int index = 1;
+        for (BankAccount account : accounts) {
+            System.out.println(index + ". " + account.getName());
+            index++;
+        }
+        while(true) {
+            System.out.print("Please select an account: ");
+            int accountIndex = scanInt() - 1;
+            if (accountIndex < 0 || accountIndex >= accounts.size()) {continue;}
+            curAccount = accountIndex;
+            break;
+        }
     }
 
     public void run() {
+        while (true) {
+            doSelectedAction();
+            if (exit) {return;}
+        }
+    }
+
+    public void doSelectedAction() {
         int selection = -1;
-        while(selection != EXIT_SELECTION) {
-            displayOptions();
-            selection = getUserSelection(MAX_SELECTION);
-            processInput(selection);
+        displayOptions();
+        if (accounts.size() == EMPTY) {
+            selection = getUserSelection(startSelections.MAX.ordinal());
+            doStartSelection(selection);
+        } else {
+            selection = getUserSelection(accountSelections.MAX.ordinal());
+            doAccountSelection(selection);
+        }
+    }
+
+    public void doStartSelection(int selection) {
+        switch (startSelections.values()[selection]) {
+            case startSelections.CREATE: createAccount(); break;
+            case startSelections.EXIT: exit = true; break;
+            default: assert(false);
+        }
+    }
+
+    public void doAccountSelection(int selection) {
+        switch (accountSelections.values()[selection]) {
+            case accountSelections.DEPOSIT: performDeposit(); break;
+            case accountSelections.SWITCH: switchAccounts(); break;
+            case accountSelections.CREATE: createAccount(); break;
+            case accountSelections.EXIT: exit = true; break;
+            default: assert(false);
         }
     }
 
