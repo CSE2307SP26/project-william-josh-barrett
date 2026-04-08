42fb8076dc5b89326152849457bf7aff8d4489bc
William Vargas <vargas@wustl.edu>
2026-04-07 21:35:23 -0500

    Users can now purchase securities with their bank balance

--- runApp.sh
+++ runApp.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cd src/main
-javac MainMenu.java BankAccount.java BankManager.java
+javac MainMenu.java BankAccount.java BankManager.java BrokerMenu.java Security.java
 cd ..
 java main.MainMenu
 rm main/MainMenu.class
 rm main/BankAccount.class
 rm main/BankManager.class
+rm main/BrokerMenu.class
+rm main/Security.class
 rm main/MainMenu\$accountSelections.class
 rm main/MainMenu\$adminSelections.class
 rm main/MainMenu\$startSelections.class
