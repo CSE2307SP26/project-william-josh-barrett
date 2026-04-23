5f579a8218c162182ad95bec72a54831c83b6e37
William Vargas <vargas@wustl.edu>
2026-04-22 19:57:32 -0500

    Tiny import fix

--- src/test/BankManagerTest.java
+++ src/test/BankManagerTest.java
@@ -3,7 +3,7 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.Assert.assertThrows;
 
 import java.util.ArrayList;
 
